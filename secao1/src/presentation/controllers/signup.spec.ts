b92bcff2d75714280d1e159533faba8cd68bf763
joaorueda <joaomarcosrueda@gmail.com>
2024-11-23 22:55:24 -0300

    test: add SignUpController test

---
+++ secao1/src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { describe, test, expect } from "@jest/globals";
+import { SignupController } from "./signup";
+
+describe('SignUp Controller', () => {
+    test('shoult return 400 if no name is provided', () => {
+        const sut = new SignupController()
+        const httpRequest = {
+            body: {
+                //name: 'any_name',
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }            
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
